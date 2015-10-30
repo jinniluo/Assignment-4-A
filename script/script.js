609e6b4fed9a54b71423da2db5cd0a5a342285a1
Jinni Luo <KiniLuo@Jinnis-MacBook-Pro.local>
2015-10-29 23:46:24 -0400

    assigment4

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 var margin = {t:50,r:125,b:50,l:125};
-var width = document.getElementById('plot') - margin.r - margin.l,
-    height = document.getElementById('plot') - margin.t - margin.b;
+var width = document.getElementById('plot').clientWidth - margin.r - margin.l,
+    height = document.getElementById('plot').clientHeight - margin.t - margin.b;
 
 var canvas = d3.select('.plot')
     .append('svg')
@@ -18,31 +18,105 @@ d3.csv('data/olympic_medal_count.csv', parse, dataLoaded);
 
 function dataLoaded(err,rows){
 
-    var year = 1900;
+
+    /*var year = 1900;
     rows.sort(function(a,b){
-        //Note: this is called a "comparator" function
-        //which makes sure that the array is sorted from highest to lowest
         return b[year] - a[year];
     });
-
-    //Note: this returns "top5" as a subset of the larger array "rows", containing positions 0,1,2,3,4
     var top5 = rows.slice(0,5);
+    draw(top5, year);*/
 
-    //Call the draw function
-    draw(top5, year);
+
 
     //TODO: fill out this function
     d3.selectAll('.btn-group .year').on('click',function(){
 
-        var year = d3.select(this).id();
+
+
+        var year = d3.select(this).attr('id');
+
+
+
+        if (year=='year-1900')
+        {   var year = 1900;
+            rows.sort(function(a,b){
+                return b[year] - a[year];
+            });
+            var top5 = rows.slice(0,5);
+            draw(top5, year);}
+
+        else if (year =='year-1960'){
+            var year = 1960;
+            rows.sort(function(a,b){
+                return b[year] - a[year];
+            });
+            var top5 = rows.slice(0,5);
+            draw(top5, year);
+        }
+        else if(year=='year-2012'){
+            var year = 2012;
+            rows.sort(function(a,b){
+                return b[year] - a[year];
+            });
+            var top5 = rows.slice(0,5);
+            draw(top5, year);
+
+        }
         console.log("Show top 5 medal count for: " + year);
+        console.log(top5);
     });
 }
+
+
+
+
 
 function draw(rows, year){
     //TODO: Complete drawing function, accounting for enter, exit, update
     //Note that this function requires two parameters
     //The second parameter, "year", determines which one of the three years (1900,1960,2012) to draw the medal counts based on
+    //update selection
+    var nodes = canvas.selectAll('.country')
+        .data(rows, function(d){
+            return d.country;
+        });
+
+
+    //enter set
+    var nodesEnter = nodes.enter()
+        .append('g')
+        .attr('class','country')
+        .attr('transform','translate('+width/2+','+height/2+')');
+        /*.attr('transform',function(d,index){
+            return width/5*index;
+        });*/
+
+    nodesEnter
+        .append('circle')
+        .attr('r',0)
+        .style('fill','rgba(80,80,80,.1)')
+        .style('stroke','rgb(50,50,50)')
+        .style('stroke-width','1px');
+
+
+    nodesEnter
+        .append('text')
+        .text(function(d){return d.country});
+
+
+    //exit
+    nodes.exit().remove();
+
+// update set
+    nodes
+        .transition()
+        .select('circle')
+        .attr('r',function(d){return scaleR(d[year])})
+
+
+
+
+
 }
 
 function parse(row){
